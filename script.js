7dd79478efd6dfba60161ed4ea2b0a50faf8812f
isaquegth <isaque.guedes@escola.pr.gov.br>
2025-06-02 11:48:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('formulario').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Obrigado por entrar em contato! 🌱');
+    this.reset();
+  });
+  
